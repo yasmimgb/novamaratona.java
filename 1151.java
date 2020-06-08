072eb87560014eb6b426009d01e1edf1c81affcc
yasmimgb <63204225+yasmimgb@users.noreply.github.com>
2020-06-08 16:59:57 -0300

    Easy Fibonacci

---
+++ 1151.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class URI_1151 {
+
+	public static void main(String[] args) {
+		int n1=0,n2=1,n3,i,N; 
+		 
+	    Scanner input =new Scanner(System.in);
+	    N = input.nextInt();
+		 System.out.print(n1+" "+n2);  
+		 for(i=2;i<N;++i)  
+		 {    
+		  n3=n1+n2;    
+		  System.out.print(" "+n3);    
+		  n1=n2;    
+		  n2=n3;    
+		 }    
+		 System.out.print("\n");
+	}
+
+}
