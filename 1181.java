8419929447220ae2844b1c9a351414f145a3c17f
yasmimgb <63204225+yasmimgb@users.noreply.github.com>
2020-06-08 17:37:51 -0300

    Line in Array

---
+++ 1181.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MyClass {
+	public static void main(String[] args) {		
+
+		Scanner sc = new Scanner(System.in);
+		
+		float[][] M = new float[12][12];
+		System.out.println("Digite a linha que deseja Somar ou tirar Media");
+		int L = sc.nextInt();
+		
+		if ((L <= 0) || (L >= 11)) {
+			System.out.println("Numero deve ser entre 0 e 11");
+			System.exit(0);			
+		}
+		
+		System.out.println("Digite se deseja tirar media(M) ou se deseja Somar(S)");
+		String T = sc.nextLine();
+		
+		if (T != "M" || T != "S") {
+			System.out.println("Só deve ser inserido S ou M");
+			System.exit(0);						
+		}
+		
+		System.out.println("digite o valor da tabela");
+		
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				M[i][j] = sc.nextFloat();
+			}			
+		}
+
+		float soma = 0;
+				
+		for (int i = 0; i < 11; i++) {
+			soma += M[L][i];										
+		}
+		
+		if(T == "S") {
+			System.out.println(soma);			
+		}else if (T == "M") {
+			System.out.println(soma / 11);			
+		}				
+		
+		sc.close();
+	}
+
+}
